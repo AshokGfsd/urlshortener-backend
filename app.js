0b0daeb1f0db74192283f8c06ea846f0c861076a
AshokGfsd <166788378+AshokGfsd@users.noreply.github.com>
2024-07-10 12:28:38 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+const userRouter = require("./routes/userRoutes");
+const urlRouter = require("./routes/urlRoutes");
+const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
+const requestLogger = require("./utils/logger");
+const unknownEndpoint = require("./utils/Error");
+
+app = express();
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(morgan("dev"));
+
+app.use("/users", requestLogger, userRouter);
+app.use("/url", requestLogger, urlRouter);
+app.use(unknownEndpoint);
+
+module.exports = app;
